d7a57132dbca3dcfacf4c3d174d56bc8d6522c30
Steven A White <sawhite@ara.com>
2020-09-08 16:25:35 -0400

    f/sawhite-cmake-update -- This commit includes several back ported CMake fixes and code layout changes from core to make it easier to maintain. I got a little ahead of myself making splitting up the code chantges hard in to small atomic commits. A list of most of the major changes is included below.
        Adding worktrees to gitignore to enhance workflow
    
        Adding bg-cli and bg-scenario from core to core-lite
        Additionally removing JAVA code which is no longer maintained.
        bg-cli depends on minzip so source is included per the license to remove
        complications when building. Hopefully I added it in away to avoid the zip.h issues we have
        in core.
    
        Renamed STAGE to BUNDLE_DLLs moved BUNDLE_DLLs msvc folder to Other
        Renamed stage_runtime to prepare_runtime_dir to match core
    
    Updated CMake to be closer to core
    -- project/biogears has separate circuit test library
    -- All libraries are now libBiogearsLite
    -- Auto Versioning from Git libBIogearsLIte defaults v0.5 until we decide what to call it
    -- Compilation Speed Improvments for Build Machine

--- cmake/cmake-common_stage.cmake
+++ cmake/cmake-common_stage.cmake
@@ -1,6 +1,6 @@
 include(BundleUtilities)
 include(cmake-common_cache.cmake)
-include(${PROJECT_SOURCE_DIR}/cmake/cmake-common_logic.cmake)
+include("${PROJECT_SOURCE_DIR}/cmake/cmake-common_logic.cmake")
 ############/############################################################################################
 #
 # Source File Managment Macros
@@ -15,6 +15,19 @@ function(CHILDLIST result curdir)
   endforeach()
   set(${result} ${dirlist} PARENT_SCOPE)
 endfunction()
+
+function(EXELIST result curdir)
+  file(GLOB children RELATIVE ${curdir} ${curdir}/*)
+  set(dirlist "")
+  foreach(child ${children})
+    if(NOT IS_DIRECTORY ${curdir}/${child} AND ${child} MATCHES ".*${CMAKE_EXECUTABLE_SUFFIX}" )
+      list(APPEND dirlist ${curdir}/${child})
+	  set(${result} ${dirlist} PARENT_SCOPE)
+	  return()
+    endif()
+  endforeach()
+endfunction()
+
 
 function(add_source_files var prefix regex source_group)
     message(STATUS "add_source_files( ${var} \"${prefix}\" ${regex} \"${source_group}\")")
@@ -45,26 +58,17 @@ endif()
 
 set(CONFIG_SUFFIX ${CMAKE_${CONFIG}_POSTFIX})
 foreach(_dir IN LISTS CMAKE_PREFIX_PATH)
-    list(APPEND THIRD_PARTY  ${_dir})
-    list(APPEND THIRD_PARTY_LIB ${_dir}/lib)
-    list(APPEND THIRD_PARTY_BIN ${_dir}/bin)
+    list(APPEND THIRD_PARTY  "${_dir}")
+    list(APPEND THIRD_PARTY_LIB "${_dir}/lib")
+    list(APPEND THIRD_PARTY_BIN "${_dir}/bin")
 endforeach()
 
-CHILDLIST(projects ${PROJECT_SOURCE_DIR}/projects )
+
+EXELIST(projects ${CMAKE_RUNTIME_OUTPUT_DIRECTORY} )
 
 foreach( project IN LISTS projects)
-  if (EXISTS ${project}/ext/cmake/bundle.cmake)
-    message(STATUS "Bundling ${project}" )
-    include(${project}/ext/cmake/bundle.cmake)
-  endif()
-  get_filename_component(project_name ${project} NAME )
-  if ( project_name STREQUAL examples ) 
-    CHILDLIST(examples ${project} )
-    foreach( example IN LISTS examples)
-      if(EXISTS ${example}/ext/cmake/bundle.cmake)
-         message(STATUS "Bundling ${example}" )  
-         include(${example}/ext/cmake/bundle.cmake)
-      endif()
-    endforeach()
-  endif()
+	fixup_bundle(${project}
+                  ""
+                 "${THIRD_PARTY_LIB};${THIRD_PARTY_BIN}"
+	  )
 endforeach()
